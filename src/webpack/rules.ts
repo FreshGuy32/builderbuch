e9c714a50352c81ec5c88c2561eed7857c82bd25
FreshGuy32 <mathias.trapl@gmail.com>
2020-03-16 17:12:10 +0100

    Added babel stuff

--- src/webpack/rules.ts
+++ src/webpack/rules.ts
@@ -2,30 +2,30 @@ import { IConfigOptions } from './types'
 
 export const rules = ({ mode }: IConfigOptions) => [
     {
-        test: /\.tsx?$/i,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.(js|ts)x?$/i,
+        exclude: /node_modules/,
         use: {
             loader: 'babel-loader',
             options: {
-                extends: './.babelrc',
+                extends: '../../.babelrc',
                 envName: mode,
             },
         },
     },
-    {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-    },
-    {
-        test: /\.svg$/,
-        use: [
-            {
-                loader: '@svgr/webpack',
-                options: {
-                    svgo: true,
-                },
-            },
-            'file-loader',
-        ],
-    },
+    // {
+    //     test: /\.css$/i,
+    //     use: ['style-loader', 'css-loader'],
+    // },
+    // {
+    //     test: /\.svg$/,
+    //     use: [
+    //         {
+    //             loader: '@svgr/webpack',
+    //             options: {
+    //                 svgo: true,
+    //             },
+    //         },
+    //         'file-loader',
+    //     ],
+    // },
 ]
