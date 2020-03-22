03a8be0802cd395b530295cfb73b8597a34e9695
FreshGuy32 <mathias.trapl@gmail.com>
2020-03-22 10:31:35 +0100

    Moved creation of  compiler into seperate function.

--- src/build.ts
+++ src/build.ts
@@ -1,31 +1,12 @@
-import webpack from 'webpack'
-import createConfig from './webpack/webpack.config'
 import { argv } from './helper/parsedArgs'
-import { findClosestPackageFile } from './helper/findClosestPackageFile'
-import { readFile } from 'fs-extra'
 import clearConsole from 'react-dev-utils/clearConsole'
+import { createCompiler } from './helper/createCompiler'
 ;(async () => {
-    let publicPath = '/'
-
-    const closestPackageJsonPath = await findClosestPackageFile(process.cwd())
-    if (closestPackageJsonPath) {
-        const content = await readFile(closestPackageJsonPath)
-
-        const stringContent = content.toString()
-        const jsonContent = JSON.parse(stringContent) as { homepage?: string }
-        if (jsonContent.homepage) {
-            publicPath = jsonContent.homepage
-        }
-    }
-
-    const compiler = webpack(createConfig({ ...argv, publicPath }))
+    const compiler = await createCompiler()
 
     await new Promise(resolve => {
         if (argv.watch) {
-            const watcher = compiler.watch({}, (_, stats) => {
-                clearConsole()
-                console.log(stats.toString({ colors: true }))
-            })
+            const watcher = compiler.watch({}, () => clearConsole())
 
             process.on('SIGINT', () => {
                 watcher.close(() => {
@@ -35,10 +16,7 @@ import clearConsole from 'react-dev-utils/clearConsole'
                 })
             })
         } else {
-            compiler.run((_, stats) => {
-                console.log(stats.toString({ colors: true }))
-                resolve()
-            })
+            compiler.run(() => resolve())
         }
     }).then()
 })()
---
+++ src/helper/createCompiler.ts
@@ -0,0 +1,44 @@
+import webpack from 'webpack'
+import createConfig from '../webpack/webpack.config'
+import { findClosestPackageFile } from '../helper/findClosestPackageFile'
+import { readFile } from 'fs-extra'
+import formatWebpackMessages from 'react-dev-utils/formatWebpackMessages'
+import { argv } from '../helper/parsedArgs'
+
+export const createCompiler = async () => {
+    let publicPath = '/'
+
+    const closestPackageJsonPath = await findClosestPackageFile(process.cwd())
+    if (closestPackageJsonPath) {
+        const content = await readFile(closestPackageJsonPath)
+
+        const stringContent = content.toString()
+        const jsonContent = JSON.parse(stringContent) as { homepage?: string }
+        if (jsonContent.homepage) {
+            publicPath = jsonContent.homepage
+        }
+    }
+    const compiler = webpack(createConfig({ ...argv, publicPath }))
+    compiler.hooks.invalid.tap('invalid', function () {
+        console.log('Compiling...')
+    })
+
+    compiler.hooks.done.tap('done', function (stats) {
+        const rawMessages = stats.toJson({}, true)
+        const messages = formatWebpackMessages(rawMessages)
+        if (!messages.errors.length && !messages.warnings.length) {
+            console.log('Compiled successfully!')
+        }
+        if (messages.errors.length) {
+            console.log('Failed to compile.')
+            messages.errors.forEach(e => console.log(e))
+            return
+        }
+        if (messages.warnings.length) {
+            console.log('Compiled with warnings.')
+            messages.warnings.forEach(w => console.log(w))
+        }
+    })
+
+    return compiler
+}
