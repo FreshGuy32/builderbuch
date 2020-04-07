8125c75c3fcee3fed7f1b70eef6399c25117952d
FreshGuy32 <mathias.trapl@gmail.com>
2020-04-07 16:53:50 +0200

    smth

--- src/helper/createCompiler.ts
+++ src/helper/createCompiler.ts
@@ -14,6 +14,7 @@ export const createCompiler = async (args: PossibleArguments) => {
 
         const stringContent = content.toString()
         const jsonContent = JSON.parse(stringContent) as { homepage?: string }
+
         if (jsonContent.homepage) {
             publicPath = jsonContent.homepage
         }
