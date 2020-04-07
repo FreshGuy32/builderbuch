2c04fda7a80eaa4baa042af542e2eb1309f9832e
FreshGuy32 <mathias.trapl@gmail.com>
2020-04-07 16:14:41 +0200

    Removed generated files that were accidentaly commited.

--- dist/webpack/types.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="webpack-dev-server" />
-import { Configuration } from 'webpack';
-export interface IConfigOptions {
-    a: boolean;
-    environment: 'server' | 'local';
-    mode: Configuration['mode'];
-}
