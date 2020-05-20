fc1825373cf955e5f8a7928c39f2fed93d42fbad
FreshGuy32 <mathias.trapl@gmail.com>
2020-05-20 10:24:45 +0200

    refactor(builderbuch_cli/core): moved IExtensionModule into core package

--- packages/cli/src/helper/loadExtensions.ts
+++ packages/cli/src/helper/loadExtensions.ts
@@ -1,13 +1,6 @@
 import { pathExists } from 'fs-extra'
-import {
-    ExtensionPlugins,
-    ExtensionRules,
-} from '@freshguy32/builderbuch_core/src/types/extendability'
+import { IExtensionModule } from '@freshguy32/builderbuch_core/src/types/extendability'
 
-interface IExtensionModule {
-    plugins: ExtensionPlugins
-    rules: ExtensionRules
-}
 export const loadExtensions = async (
     path: string
 ): Promise<IExtensionModule | undefined> => {
