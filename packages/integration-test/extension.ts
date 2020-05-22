4b9d96275c6f7a22d20bcf8de2572c1b70293ff1
FreshGuy32 <mathias.trapl@gmail.com>
2020-05-22 17:47:03 +0200

    test(builderbuch_integration-test): added new test package
    this package can be used to test builderbuch in a real project environment, without needing to publish it first

---
+++ packages/integration-test/extension.ts
@@ -0,0 +1,18 @@
+import {
+    ExtensionPlugins,
+    ExtensionRules,
+} from '@freshguy32/builderbuch_cli/src/extendability'
+import * as HtmlWebpackPlugin from 'html-webpack-plugin'
+import { resolve } from 'path'
+
+export const plugins: ExtensionPlugins = basePath => [
+    {
+        mode: 'override',
+        type: 'plugins',
+        name: 'HtmlWebpackPlugin',
+        value: new HtmlWebpackPlugin({
+            title: 'Integration Test',
+            template: resolve(basePath, 'template.html'),
+        }),
+    },
+]
---
+++ packages/integration-test/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const test = 'Hello World'
+
+ReactDOM.render(<h1>{test}</h1>, document.querySelector('#root'))
