40e0b8f26fe8867e300e3014f14334e1ea2628a1
FreshGuy32 <mathias.trapl@gmail.com>
2020-05-28 20:04:03 +0200

    chore(builderbuch_test): added private to test package

--- packages/test/src/index.tsx
+++ packages/test/src/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-class MyTest {}
-
 const test = 'Hello World'
 
 ReactDOM.render(<h1>{test}</h1>, document.querySelector('#root'))
