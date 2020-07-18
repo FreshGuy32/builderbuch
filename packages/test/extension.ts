060178b555ea4b06e98c994db32c37a158cf8e87
FreshGuy32 <mathias.trapl@gmail.com>
2020-07-18 16:55:46 +0200

    test(test): fixed extension plugins in test project not being correct after latest change

--- packages/test/extension.ts
+++ packages/test/extension.ts
@@ -3,7 +3,7 @@ import { ExtensionPlugins } from '@freshguy32/builderbuch_cli/src/extendability'
 import * as HtmlWebpackPlugin from 'html-webpack-plugin'
 import { resolve } from 'path'
 
-export const plugins: ExtensionPlugins = basePath => [
+export const plugins: ExtensionPlugins = ({basePath}) => [
     {
         mode: 'override',
         type: 'plugins',
