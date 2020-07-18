cfb1fd2be6221ec56873573d36c8f4a0b9baffa2
FreshGuy32 <mathias.trapl@gmail.com>
2020-07-18 17:19:26 +0200

    style(test): fixed spacing

--- packages/test/extension.ts
+++ packages/test/extension.ts
@@ -1,9 +1,9 @@
 import { ExtensionPlugins } from '@freshguy32/builderbuch_cli/src/extendability'
 
-import * as HtmlWebpackPlugin from 'html-webpack-plugin'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 import { resolve } from 'path'
 
-export const plugins: ExtensionPlugins = ({basePath}) => [
+export const plugins: ExtensionPlugins = ({ basePath }) => [
     {
         mode: 'override',
         type: 'plugins',
