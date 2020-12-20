36cfd21c042bd4943186657366116bd8d7ebf6dd
Mathias Trapl <mathias@mogi.at>
2020-12-20 15:33:38 +0100

    feat(core): removed not needed type from ExtendableAddition/-Override

--- packages/core/src/types/extendability.ts
+++ packages/core/src/types/extendability.ts
@@ -5,27 +5,23 @@ export type ExtensionType = 'plugins' | 'rules'
 
 export type OverrideablePluginNames = 'HtmlWebpackPlugin'
 export type OverrideableRuleNames = 'styles'
-export type ExtendableAdditon<Type extends ExtensionType, Value> = {
-    type: Type
+export type ExtendableAdditon<Value> = {
     mode: 'addition'
     value: Value
 }
-export type ExtendableOverride<Type extends ExtensionType, Value, Name> = {
-    type: Type
+export type ExtendableOverride<Value, Name> = {
     mode: 'override'
     name: Name
     value: Value
 }
 
-export type ExtensionPluginAdditon = ExtendableAdditon<'plugins', Plugin>
+export type ExtensionPluginAdditon = ExtendableAdditon<Plugin>
 export type ExtensionPluginOverride = ExtendableOverride<
-    'plugins',
     Plugin,
     OverrideablePluginNames
 >
-export type ExtensionRuleAdditon = ExtendableAdditon<'rules', RuleSetRule>
+export type ExtensionRuleAdditon = ExtendableAdditon<RuleSetRule>
 export type ExtensionRuleOverride = ExtendableOverride<
-    'rules',
     RuleSetRule,
     OverrideableRuleNames
 >
